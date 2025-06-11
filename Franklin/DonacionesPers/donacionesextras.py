f2221e02a711ba2db34b3a961ccb966f8eed469b
FICT1 <ficallejas@uamv.edu.ni>
2025-06-11 17:26:19 -0600

    Instrucciones

---
+++ Franklin/DonacionesPers/donacionesextras.py
@@ -0,0 +1,8 @@
+#Calcular si recibio algun monto extra o regalo de fuera de la iglesia
+#Se le preguntara al usuario si recibio algun monto extra o regalo de fuera de la iglesia
+#agregarlo a una opcion de menu 
+#hacerlo todo en funciones separadas y en archivos separados
+#Se ha hecho alguna donacion extra en la iglesia??
+#una bienvenida para el usuario
+
+
---
+++ Franklin/readme.md
@@ -0,0 +1,2 @@
+#Calcular si recibio algun monto extra o regalo de fuera de la iglesia
+#Se le preguntara al usuario si recibio algun monto extra o regalo de fuera de la iglesia
