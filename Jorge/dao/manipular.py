f326d178e011d761b63bf51ab4ab7c1c7d70231e
EmperorKnight <jlpicado@uamv.edu.ni>
2025-06-27 09:52:04 -0600

    Modificaciones a la parte de dao.manipular

--- Jorge/dao/manipular.py
+++ Jorge/dao/manipular.py
@@ -22,7 +22,7 @@ class Manipular:
                         except ValueError:
                             pass
 
-            print(f"Total de donaciones: C${total:.2f}")
+            print(f"Total de donaciones: C${total:,.2f}")
 
     def buscar(self, d):
         if os.path.exists("Lista.txt"):
@@ -43,29 +43,27 @@ class Manipular:
         if os.path.exists("Lista.txt"):
             with open("Lista.txt", "r", encoding="utf-8") as archivo:
                 lineas = archivo.readlines()
-                lineas = [linea.strip() for linea in lineas]
             with open("Lista.txt", "w", encoding="utf-8") as archivo:
                 for linea in lineas:
-                    if linea.startswith(f"{d})") or d in linea:
+                    if d in linea:
                         lineas.remove(linea)
-                    else:
-                        archivo.write(linea + "\n")
+                        break
+                archivo.writelines(lineas)
         else:
             print("El archivo no existe.")
-    
+
     def eliminar_en_respaldo(self,d):
         if os.path.exists("Respaldo.txt"):
             with open("Respaldo.txt", "r", encoding="utf-8") as archivo:
                 lineas = archivo.readlines()
-                lineas = [linea.strip() for linea in lineas]
             with open("Respaldo.txt", "w", encoding="utf-8") as archivo:
                 for linea in lineas:
-                    if linea.startswith(f"{d})") or d in linea:
+                    if d in linea:
                         lineas.remove(linea)
-                    else:
-                        archivo.write(linea + "\n")
+                        break
+                archivo.writelines(lineas)
         else:
             print("El archivo de respaldo no existe.")           
-                        
+
     def mostrar(self):
         return self.lista
